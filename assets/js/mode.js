7fa3e34ccc056e32132b165ec6542cdc87c27f78
Krishna-Singhal <659028764+Krishna-Singhal@users.noreply.github.com>
2021-04-27 19:07:30 +0530

    trying to save user preference

--- assets/js/mode.js
+++ assets/js/mode.js
@@ -1,4 +1,18 @@
+if (localStorage.mode) {
+    if (localStorage.mode == "dark")
+        document.body.classList.toggle("dark");
+    else
+        document.body.classList.toggle("light");
+} else
+    localStorage.setItem('mode', 'light')
+
 function switchMode() {
     var element = document.body;
-    element.classList.toggle("dark");
+    if (localStorage.mode == "dark") {
+        localStorage.setItem('mode', 'light')
+        element.classList.toggle("light");
+    } else {
+        localStorage.setItem('mode', 'dark')
+        element.classList.toggle("dark");
     }
+}
